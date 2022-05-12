db7b866e8818cd76787f66788accfd86ebc7ab98
Tahir Ahmad <tahirahmad.in@gmail.com>
2022-05-12 18:07:44 +0530

    stake popup adds

---
+++ src/pages/Home/components/PoolCardDisabled.js
@@ -0,0 +1,287 @@
+import React, { useState } from "react";
+import { makeStyles } from "@mui/styles";
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    backgroundImage: 'url("images/network.png"), url(images/tokens.png)',
+    backgroundPosition: "center center,center center",
+    backgroundRepeat: "no-repeat,no-repeat",
+    backgroundSize: "cover,contain",
+    height: "88vh",
+    width: "100%",
+    paddingTop: "4%",
+  },
+  mainHeading: {
+    fontWeight: 600,
+    fontSize: 48,
+    letterSpacing: "0.02em",
+    color: "#212121",
+    textAlign: "center",
+    [theme.breakpoints.down("md")]: {
+      color: "#212121",
+    },
+  },
+  poolText: {
+    fontWeight: 600,
+    fontSize: 22,
+    letterSpacing: "0.02em",
+    color: "#212121",
+    textAlign: "center",
+    [theme.breakpoints.down("md")]: {
+      color: "#212121",
+    },
+  },
+  para: {
+    fontWeight: 400,
+    fontSize: 16,
+    letterSpacing: "0.02em",
+    color: "#414141",
+    textAlign: "center",
+  },
+
+  buttonWrapper: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  buttonFirst: {
+    width: "fit-content",
+    color: "#212121",
+    backgroundColor: "#eeeeee",
+    padding: "12px 50px 12px 50px",
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    cursor: "pointer",
+  },
+  buttonSecond: {
+    width: "fit-content",
+    color: "white",
+    backgroundColor: "#6A55EA",
+    padding: "12px 50px 12px 50px",
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10,
+    cursor: "pointer",
+  },
+  filterCard: {
+    marginTop: 20,
+    marginBottom: 20,
+    height: "100%",
+    width: "80%",
+    border: "1px solid #eeeeee",
+
+    paddingTop: 30,
+    paddingBottom: 30,
+
+    backgroundColor: "#FFFFFF",
+    boxShadow: "0px 12px 24px rgba(0, 0, 0, 0.03)",
+    borderRadius: 10,
+  },
+  cardTitle: {
+    fontWeight: 400,
+    fontSize: 16,
+    letterSpacing: "0.02em",
+    color: "#414141",
+    textAlign: "left",
+  },
+}));
+
+export default function PoolCardDisabled() {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const [orderType, setOrderType] = useState("BUY");
+  const [fiat, setFiat] = useState("BTC");
+  const [token, setToken] = useState("USDT");
+  const [payment, setPayment] = useState("UPI");
+
+  const [filterParams, setFilterParams] = useState({
+    orderType: "BUY",
+    fiat: "INR",
+    token: "PBR",
+    payment: "UPI",
+  });
+
+  const updateFilters = () => {
+    let tempObj = {
+      orderType: orderType,
+      fiat: fiat,
+      token: token,
+      payment: payment,
+    };
+    setFilterParams(tempObj);
+  };
+  return (
+    <Box>
+      <div className={classes.filterCard}>
+        <Box pt={0} px={3}>
+          <Box
+            display="flex"
+            flexDirection={"row"}
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Box
+              display="flex"
+              flexDirection={"row"}
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <img
+                src="https://cdn3d.iconscout.com/3d/premium/thumb/chainlink-coin-4199896-3478982@0.png"
+                alt="Polygon"
+                height="20px"
+              />{" "}
+              <img
+                src="https://cdn3d.iconscout.com/3d/premium/thumb/tether-4924313-4102064.png"
+                alt="USDT"
+                height="20px"
+              />
+            </Box>
+            <Typography
+              variant="body2"
+              className={classes.para}
+              textAlign="left"
+              fontWeight={600}
+              ml={1}
+            >
+              LINK/USDT
+            </Typography>
+          </Box>
+          <Box display={"flex"} justifyContent={"space-around"} mt={3}>
+            <Box>
+              <Typography
+                variant="h6"
+                className={classes.para}
+                textAlign="center"
+                fontSize={14}
+                fontWeight={400}
+                ml={1}
+              >
+                Total Deposits
+              </Typography>
+              <Typography
+                variant="body2"
+                className={classes.para}
+                textAlign="center"
+                fontWeight={700}
+                ml={1}
+              >
+                $0
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                variant="h6"
+                className={classes.para}
+                textAlign="center"
+                fontSize={14}
+                fontWeight={400}
+                ml={1}
+              >
+                Avg Gains
+              </Typography>
+              <Typography
+                variant="body2"
+                className={classes.para}
+                textAlign="center"
+                fontWeight={700}
+                ml={1}
+              >
+                0
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              mt={3}
+              style={{
+                border: "1px solid rgba(106, 85, 234,0.1)",
+                padding: "10px 10px 10px 10px",
+                borderRadius: 10,
+                backgroundColor: "rgba(106, 85, 234,0.03)",
+              }}
+            >
+              <Typography
+                variant="h6"
+                className={classes.para}
+                textAlign="center"
+                fontSize={14}
+                fontWeight={400}
+                ml={1}
+              >
+                Your stake
+              </Typography>
+              <Typography
+                variant="body2"
+                className={classes.para}
+                textAlign="center"
+                fontWeight={700}
+                ml={1}
+              >
+                $0
+              </Typography>
+            </Box>
+            <Box
+              display={"flex"}
+              justifyContent={"space-between"}
+              mt={2}
+              style={{
+                border: "1px solid rgba(106, 85, 234,0.1)",
+                padding: "10px 10px 10px 10px",
+                borderRadius: 10,
+                backgroundColor: "rgba(106, 85, 234,0.03)",
+              }}
+            >
+              <Typography
+                variant="h6"
+                className={classes.para}
+                textAlign="center"
+                fontSize={14}
+                fontWeight={400}
+                ml={1}
+              >
+                Earning
+              </Typography>
+              <Typography
+                variant="body2"
+                className={classes.para}
+                textAlign="center"
+                fontWeight={700}
+                ml={1}
+              >
+                $0
+              </Typography>
+            </Box>
+          </Box>
+          <Box px={2} mt={2} className="text-center">
+            <Button
+              disabled={true}
+              style={{
+                borderRadius: 10,
+                background: "rgba(106, 85, 234,0.6)",
+                padding: "9px 20px 9px 20px",
+                color: "white",
+              }}
+            >
+              Coming Soon
+            </Button>
+          </Box>
+        </Box>
+      </div>
+    </Box>
+  );
+}
---
+++ src/pages/Home/components/StakePopup.js
@@ -0,0 +1,536 @@
+//done
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Dialog,
+  Typography,
+  Slide,
+  Backdrop,
+  Box,
+  Input,
+  Grid,
+  Slider,
+} from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import { Close } from "@mui/icons-material";
+import { useMoralis, useMoralisWeb3Api } from "react-moralis";
+import Web3 from "web3";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const useStyles = makeStyles((theme) => ({
+  background: {
+    position: "fixed",
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    width: "100%",
+    height: "100%",
+    zIndex: 10,
+    display: "grid",
+    placeItems: "center",
+    background: "rgba(0,0,0,0.2)",
+  },
+  container: {
+    width: "100%",
+    height: "fit-content",
+    padding: 10,
+    minHeight: 400,
+    maxWidth: 500,
+    position: "relative",
+    background: "#fff",
+    border: "15px solid #6A55EA",
+    display: "flex",
+    alignItems: "center",
+    zIndex: 11,
+    [theme.breakpoints.down("md")]: {
+      border: "10px solid #D1FE1D",
+      width: "100%",
+      maxWidth: "95%",
+      height: 350,
+    },
+    [theme.breakpoints.down("sm")]: {
+      height: "max-content",
+    },
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 8,
+    right: 8,
+    height: 22,
+    width: 22,
+    cursor: "pointer",
+    [theme.breakpoints.down("md")]: {
+      top: 5,
+      right: 5,
+      height: 18,
+      width: 18,
+    },
+  },
+  inputWrapper: {
+    padding: 10,
+  },
+  input: {
+    backgroundColor: "#ffffff",
+    border: "1px solid #757575",
+    borderRadius: 18,
+    width: "80%",
+    padding: 6,
+    outline: "none",
+    color: "#212121",
+    textAlign: "left",
+    paddingLeft: 10,
+    paddingTop: 8,
+    paddingBottom: 8,
+    fontSize: 14,
+    fontFamily: "Karla",
+  },
+  heading: {
+    color: "#000000",
+    textAlign: "center",
+    fontSize: 30,
+    lineHeight: "20%",
+    [theme.breakpoints.down("md")]: {
+      fontSize: 24,
+    },
+  },
+
+  para: {
+    color: "#212121",
+    textAlign: "center",
+    fontSize: 13,
+    fontWeight: 300,
+    paddingTop: 5,
+    [theme.breakpoints.down("md")]: {
+      fontSize: 13,
+      paddingTop: 15,
+    },
+  },
+  activateButton: {
+    width: "fit-content",
+    height: "50px",
+    background: "#FF5AFF",
+    boxSizing: "border-box",
+    borderRadius: "15px",
+    fontSize: 16,
+    lineHeight: "33px",
+    color: "#ffffff",
+    fontWeight: 700,
+
+    padding: "12px 30px 12px 30px",
+    "&:hover": {
+      background: "#FFB469",
+    },
+    [theme.breakpoints.down("md")]: {
+      padding: "12px 20px 12px 20px",
+      fontSize: 18,
+    },
+  },
+  connectButton: {
+    width: "fit-content",
+
+    background: theme.palette.primary.main,
+    border: "1px solid #FFFFFF",
+    boxSizing: "border-box",
+    borderRadius: "15px",
+    fontSize: 16,
+    lineHeight: "33px",
+    color: "#ffffff",
+    fontWeight: 600,
+    marginTop: 20,
+    padding: "10px 40px 10px 40px",
+    "&:hover": {
+      background: theme.palette.primary.main,
+    },
+    [theme.breakpoints.down("md")]: {
+      padding: "12px 20px 12px 20px",
+      fontSize: 18,
+    },
+  },
+  registerButton: {
+    width: "fit-content",
+    height: "45px",
+    background: "#FF87FF",
+    border: "1px solid #FFFFFF",
+    boxSizing: "border-box",
+    borderRadius: "20px",
+    fontSize: 16,
+    lineHeight: "33px",
+    color: "#000000",
+
+    marginTop: 20,
+    padding: "12px 30px 12px 30px",
+    "&:hover": {
+      background: "#FFB469",
+    },
+    [theme.breakpoints.down("md")]: {
+      padding: "12px 20px 12px 20px",
+      fontSize: 18,
+    },
+  },
+
+  svgImage: {
+    width: 100,
+    [theme.breakpoints.down("md")]: {
+      width: 70,
+    },
+  },
+  spoon1Image: {
+    width: 40,
+    height: "fit-content",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  },
+  spoon2Image: {
+    width: 30,
+    height: "fit-content",
+    [theme.breakpoints.down("md")]: {
+      display: "none",
+    },
+  },
+  iconWrapper: {
+    marginRight: 10,
+    backgroundColor: "#FF87FF",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: 42,
+    height: 42,
+  },
+  icon: {
+    width: 25,
+    height: 25,
+    color: "white",
+  },
+}));
+
+const StakePopup = ({ stakePopup, setStakePopup }) => {
+  const classes = useStyles();
+
+  const { user, logout } = useMoralis();
+
+  const Web3Api = useMoralisWeb3Api();
+  const userAddress = user ? user.attributes.ethAddress : "...";
+
+  const [amount, setAmount] = useState(0);
+  const [percent, setPercent] = useState(0);
+  const [grids, setGrids] = useState(0);
+
+  const resetPopup = () => {
+    setStakePopup(false);
+  };
+
+  const marks = [
+    {
+      value: 0,
+      label: "2",
+    },
+    {
+      value: 20,
+      label: "6",
+    },
+    {
+      value: 50,
+      label: "10",
+    },
+    {
+      value: 75,
+      label: "14",
+    },
+    {
+      value: 100,
+      label: "20",
+    },
+  ];
+
+  function valuetext(value) {
+    return `${value}°C`;
+  }
+  return (
+    <Dialog
+      open={stakePopup}
+      TransitionComponent={Transition}
+      keepMounted={false}
+      onClose={null}
+      closeAfterTransition
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+      maxWidth="lg"
+      fullWidth={false}
+    >
+      <div className={classes.background}>
+        <div className={classes.container}>
+          <div className="h-100 w-100">
+            <div className="d-flex justify-content-end" onClick={resetPopup}>
+              <Close style={{ cursor: "pointer" }} />
+            </div>
+            <div className="d-flex flex-column justify-content-around">
+              <div>
+                <Typography
+                  variant="h4"
+                  className={classes.heading}
+                  fontWeight={700}
+                >
+                  Stake
+                </Typography>
+                <Box
+                  display="flex"
+                  flexDirection={"row"}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Box
+                    display="flex"
+                    flexDirection={"row"}
+                    justifyContent="flex-start"
+                    alignItems="center"
+                  >
+                    <img
+                      src="https://cdn3d.iconscout.com/3d/premium/thumb/polygon-4924309-4102060.png"
+                      alt="Polygon"
+                      height="20px"
+                    />{" "}
+                    <img
+                      src="https://cdn3d.iconscout.com/3d/premium/thumb/tether-4924313-4102064.png"
+                      alt="USDT"
+                      height="20px"
+                    />
+                  </Box>
+                  <Typography
+                    variant="body2"
+                    className={classes.para}
+                    textAlign="left"
+                    fontWeight={600}
+                    ml={1}
+                  >
+                    MATIC/USDT
+                  </Typography>
+                </Box>
+              </div>
+
+              <Box
+                display={"flex"}
+                justifyContent={"space-between"}
+                mt={2}
+                style={{
+                  border: "1px solid rgba(106, 85, 234,0.2)",
+                  padding: "6px 20px 6px 20px",
+
+                  borderRadius: 10,
+                  backgroundColor: "rgba(106, 85, 234,0.03)",
+                }}
+              >
+                <Box>
+                  <Typography
+                    variant="body2"
+                    textAlign={"left"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={12}
+                    color={"#757575"}
+                  >
+                    Amount:
+                  </Typography>
+                  <Input
+                    disableUnderline
+                    fullWidth
+                    placeholder="0"
+                    style={{ fontSize: 24, fontWeight: 600 }}
+                  />
+                </Box>
+                <Box>
+                  <Typography
+                    variant="body2"
+                    textAlign={"left"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={12}
+                    color={"#757575"}
+                  >
+                    Available: 3,235
+                  </Typography>
+                  <Box
+                    display="flex"
+                    flexDirection={"row"}
+                    justifyContent="center"
+                    alignItems="center"
+                  >
+                    <Box
+                      display="flex"
+                      flexDirection={"row"}
+                      justifyContent="flex-start"
+                      alignItems="center"
+                    >
+                      <img
+                        src="https://cdn3d.iconscout.com/3d/premium/thumb/usdt-coin-4999518-4160019.png"
+                        alt="USDT"
+                        height="30px"
+                      />{" "}
+                    </Box>
+                    <Typography
+                      variant="body2"
+                      className={classes.para}
+                      fontSize={20}
+                      textAlign="left"
+                      fontWeight={600}
+                      ml={1}
+                    >
+                      USDT
+                    </Typography>
+                  </Box>
+                </Box>
+              </Box>
+              <Box
+                mt={2}
+                style={{
+                  border: "1px solid rgba(106, 85, 234,0.2)",
+                  padding: "6px 30px 6px 30px",
+                  borderRadius: 10,
+                  backgroundColor: "rgba(106, 85, 234,0.03)",
+                }}
+              >
+                <Box>
+                  <Typography
+                    variant="body2"
+                    textAlign={"left"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={12}
+                    color={"#757575"}
+                  >
+                    Grids:
+                  </Typography>
+                  <Slider
+                    defaultValue={10}
+                    getAriaValueText={valuetext}
+                    step={null}
+                    marks={marks}
+                    fullWidth
+                  />
+                </Box>
+              </Box>
+              <Box></Box>
+              <Grid
+                container
+                display={"flex"}
+                justifyContent={"space-between"}
+                mt={2}
+                style={{
+                  border: "1px solid rgba(106, 85, 234,0.2)",
+                  padding: "6px 30px 6px 30px",
+                  borderRadius: 10,
+                  backgroundColor: "rgba(106, 85, 234,0.03)",
+                }}
+              >
+                <Grid item md={6} style={{ paddingRight: 10 }}>
+                  <Box
+                    style={{
+                      borderRight: "1px solid #e5e5e5",
+                      paddingRight: 10,
+                    }}
+                  >
+                    <Typography
+                      variant="body2"
+                      textAlign={"left"}
+                      className={classes.para}
+                      fontWeight={500}
+                      fontSize={12}
+                      color={"#757575"}
+                    >
+                      Grids:
+                    </Typography>
+                    <Input
+                      disableUnderline
+                      fullWidth
+                      placeholder="10"
+                      style={{ fontSize: 22, fontWeight: 600 }}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item md={6} style={{ paddingLeft: 10 }}>
+                  <Typography
+                    variant="body2"
+                    textAlign={"left"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={12}
+                    color={"#757575"}
+                  >
+                    Percent:
+                  </Typography>
+                  <Input
+                    disableUnderline
+                    fullWidth
+                    placeholder="10"
+                    style={{ fontSize: 22, fontWeight: 600 }}
+                  />
+                </Grid>
+              </Grid>
+              <Box display={"flex"} justifyContent="space-around" mt={2}>
+                <Box>
+                  <Typography
+                    variant="body2"
+                    textAlign={"center"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={14}
+                    color={"#454545"}
+                  >
+                    <strong>Your Buy Orders</strong>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    textAlign={"center"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={12}
+                    color={"#757575"}
+                  >
+                    2326, 2053,1856
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography
+                    variant="body2"
+                    textAlign={"center"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={14}
+                    color={"#454545"}
+                  >
+                    <strong>Your Sell Orders</strong>
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    textAlign={"center"}
+                    className={classes.para}
+                    fontWeight={500}
+                    fontSize={12}
+                    color={"#757575"}
+                  >
+                    2637, 2970,3246
+                  </Typography>
+                </Box>
+              </Box>
+
+              <div className="text-center">
+                <button className={classes.connectButton} onClick={logout}>
+                  STAKE NOW
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>{" "}
+    </Dialog>
+  );
+};
+
+export default StakePopup;
